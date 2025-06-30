3e9027a52ab71d5e446533ceba5cce7fdbc0ed53
hamza-halawa2020 <h.halawa2020@gmail.com>
2025-07-01 00:34:52 +0300

    update

--- src/app/pages/presentation/presentation.component.ts
+++ src/app/pages/presentation/presentation.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { Select2Module } from 'ng-select2-component';
 import { SlickCarouselModule } from 'ngx-slick-carousel';
 import { FooterComponent } from '../../components/footer/footer.component';
 import { CommonModule } from '@angular/common';
+import html2pdf from 'html2pdf.js';
+
 
 @Component({
   selector: 'app-presentation',
@@ -19,6 +21,7 @@ import { CommonModule } from '@angular/common';
   ],
 })
 export class PresentationComponent implements OnInit {
+  @ViewChild('presentationContent') presentationContent!: ElementRef;
   contactEmail = 'connect@hpsaviation.com';
   currentSlide: number = 0;
   slides: number[] = Array(8).fill(0);
@@ -48,5 +51,22 @@ export class PresentationComponent implements OnInit {
   goToSlide(index: number): void {
     this.currentSlide = index;
     this.updateProgressBar();
+  }
+
+  downloadAsPdf(): void {
+    const element = this.presentationContent.nativeElement;
+    const options = {
+      margin: 10,
+      filename: 'HPS-Aviation-Presentation.pdf',
+      image: { type: 'jpeg', quality: 0.98 },
+      html2canvas: { scale: 2 },
+      jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' },
+      pagebreak: { mode: ['avoid-all', 'css', 'legacy'] }
+    };
+
+    html2pdf(element, options)
+      .set(options)
+      .from(element)
+      .save();
   }
 }
